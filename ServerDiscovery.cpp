d51c9f99959ac744b66a00d5c0894a4efc647437
Alexandr Akulich <akulichalexander@gmail.com>
2016-04-04 16:18:25 +0500

    ServerDiscovery: Added initial implementation.

--- ServerDiscovery.cpp
+++ ServerDiscovery.cpp
@@ -1,11 +1,48 @@
 #include "ServerDiscovery.hpp"
 
-ServerDiscovery::ServerDiscovery(QObject *parent) : QObject(parent)
+#include <QUdpSocket>
+#include <QNetworkInterface>
+
+static const quint16 s_udpPort = 29031;
+static const QByteArray s_header = "IsitMessenger";
+
+ServerDiscovery::ServerDiscovery(QObject *parent) :
+    QObject(parent)
 {
+    m_udpSocket = new QUdpSocket(this);
+    m_udpSocket->bind(QHostAddress::Any, s_udpPort);
+    connect(m_udpSocket, SIGNAL(readyRead()),
+            this, SLOT(onUdpMessageReceived()));
 
 }
 
 void ServerDiscovery::addServer(quint16 port)
+{
+     m_port = port;
+}
+
+void ServerDiscovery::onUdpMessageReceived()
+{
+    while (m_udpSocket->hasPendingDatagrams()) {
+        QByteArray buffer;
+        buffer.resize(m_udpSocket->pendingDatagramSize());
+
+        QHostAddress address;
+        m_udpSocket->readDatagram(buffer.data(), buffer.size(), &address);
+    }
+}
+
+void ServerDiscovery::announceServer()
+{
+    for (QNetworkInterface iface : QNetworkInterface::allInterfaces()){
+        for (QNetworkAddressEntry entry : iface.addressEntries()) {
+            QString datagram = s_header + ":" + QString::number(m_port);
+            m_udpSocket->writeDatagram(datagram.toLatin1(), entry.broadcast(), s_udpPort);
+        }
+    }
+}
+
+void ServerDiscovery::discoveryServer()
 {
 
 }
--- ServerDiscovery.hpp
+++ ServerDiscovery.hpp
@@ -12,12 +12,20 @@ public:
     explicit ServerDiscovery(QObject *parent = 0);
 
 signals:
+    void serverFound(QString ip, quint16 port);
 
 public slots:
     void addServer(quint16 port);
 
+private slots:
+    void onUdpMessageReceived();
+
+    void announceServer();
+    void discoveryServer();
+
 private:
     QUdpSocket *m_udpSocket;
+    quint16 m_port;
 
 };
 
