51681b110c1489059d67917662b43fcbb75a2218
Alexandr Akulich <akulichalexander@gmail.com>
2016-04-04 16:58:39 +0500

    Server: Added initial implementation.

--- Server.cpp
+++ Server.cpp
@@ -1,6 +1,44 @@
 #include "Server.hpp"
 
-Server::Server(QObject *parent) : QObject(parent)
-{
+#include <QTcpServer>
+#include <QTcpSocket>
 
+Server::Server(QObject *parent) :
+    QObject(parent),
+    m_server(new QTcpServer(this))
+{
+}
+
+void Server::startServer(quint16 port)
+{
+    bool started = m_server->listen(QHostAddress::Any, port);
+
+    connect(m_server, SIGNAL(newConnection()), this, SLOT(onClientConnected()));
+
+    if (started) {
+        emit serverStarted(port);
+    }
+}
+
+void Server::sendMessage(QString message)
+{
+    for (QTcpSocket *client : m_clients) {
+        client->write(message.toUtf8());
+    }
+}
+
+void Server::onClientConnected()
+{
+    QTcpSocket *client = m_server->nextPendingConnection();
+
+    m_clients << client;
+
+    connect(client, SIGNAL(readyRead()), this, SLOT(onReadyRead()));
+}
+
+void Server::onReadyRead()
+{
+    QTcpSocket *client = qobject_cast<QTcpSocket*>(sender());
+
+    emit messageReceived(client->peerAddress().toString(), client->readAll());
 }
--- Server.hpp
+++ Server.hpp
@@ -2,6 +2,10 @@
 #define SERVER_HPP
 
 #include <QObject>
+#include <QVector>
+
+class QTcpServer;
+class QTcpSocket;
 
 class Server : public QObject
 {
@@ -10,8 +14,23 @@ public:
     explicit Server(QObject *parent = 0);
 
 signals:
+    void serverStarted(quint16 port);
+
+    void messageReceived(QString client, QString message);
 
 public slots:
+    void startServer(quint16 port);
+
+    void sendMessage(QString message);
+
+protected slots:
+    void onClientConnected();
+    void onReadyRead();
+
+private:
+    QTcpServer *m_server;
+
+    QVector<QTcpSocket*> m_clients;
 };
 
 #endif // SERVER_HPP
