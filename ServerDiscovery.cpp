8d5a453ad9300eb07760c69210dfee749d730e00
Alexandr Akulich <akulichalexander@gmail.com>
2016-04-07 20:24:45 +0500

    ServerDiscovery: Added debug info to onUdpMessageReceived().

--- ServerDiscovery.cpp
+++ ServerDiscovery.cpp
@@ -3,6 +3,8 @@
 #include <QUdpSocket>
 #include <QNetworkInterface>
 
+#include <QDebug>
+
 static const quint16 s_udpPort = 29031;
 static const QByteArray s_header = "IsitMessenger";
 
@@ -23,16 +25,19 @@ void ServerDiscovery::addServer(quint16 port)
 
 void ServerDiscovery::onUdpMessageReceived()
 {
+    qDebug() << Q_FUNC_INFO;
     while (m_udpSocket->hasPendingDatagrams()) {
         QByteArray buffer;
         buffer.resize(m_udpSocket->pendingDatagramSize());
         QHostAddress address;
         m_udpSocket->readDatagram(buffer.data(), buffer.size(), &address);
         if (buffer == s_header+":?"){
+            qDebug() << Q_FUNC_INFO << "Server requested by client" << address.toString();
             announceServer();
         } else {
             QList<QByteArray> lBuff = buffer.split(':');
             quint16 port = lBuff[1].toUInt();
+            qDebug() << Q_FUNC_INFO << "Server found at" << address.toString() << ":" << port;
             emit serverFound(address.toString(),port);
         }
     }
