1840c14aaf982d3c4b73e7db6ed6b50fa47778e1
Alexey Koshin <lexanihrom@rambler.ru>
2016-04-25 16:17:59 +0500

    исправление setNickname

--- LocalClient.cpp
+++ LocalClient.cpp
@@ -25,7 +25,7 @@ void LocalClient::sendMessage(QString message)
 
 void LocalClient::setNickname(QString nickname)
 {
-    m_socket->write("setNickname:" + nickname);
+    m_socket->write("setNickname:" + nickname.toUtf8());
     m_pendingNickname = nickname;
 }
 
